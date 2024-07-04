1b0365fd17ba6615cbfc07feb4294818af45afce
JediKnightGIT <jediknightru@gmail.com>
2024-07-04 17:26:08 +0300

    first section done

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["*.{html,js}"],
@@ -9,16 +11,41 @@ module.exports = {
     screens: {
       sm: '350px',
       md: '768px',
-      lg: '1024px',
-      xl: '1360px',
+      lg: '1360px',
+      xl: '1920px',
     },
     extend: {
       fontFamily: {
         primary: ['Gilroy', 'sans-serif'],
         secondary: ['Bebas Neue', 'sans-serif'],
         other: ['Segoe UI', 'sans-serif'],
-      }
+      },
+      colors: {
+        primary: '#31FF2D',
+        gray: '#F5F5F5',
+        darkgray: 'rgba(36, 74, 127, 0.08)',
+        // accent: {
+        //   DEFAULT: '#00FF99',
+        //   hover: '#00E187',
+        // },
+      },
+      textShadow: {
+        sm: '0 0 6px white',
+        DEFAULT: '0 2px 4px white',
+        lg: '0 8px 16px white',
+      },
     },
   },
-  plugins: [],
+  plugins: [
+    plugin(function ({ matchUtilities, theme }) {
+      matchUtilities(
+        {
+          'text-shadow': (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme('textShadow') }
+      )
+    }),
+  ],
 }
