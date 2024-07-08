6caa028bdf98e458c0d5253563bbb4132bb6b164
Jedi Knight <jediknightru@gmail.com>
2024-07-08 20:22:10 +0300

    form 70% done

--- js/calc.js
+++ js/calc.js
@@ -2,12 +2,16 @@ const deposit = document.getElementById('deposit');
 const depositInput = document.getElementById('deposit-input');
 const depositRange = document.getElementById('deposit-range');
 
-const months = document.getElementById('months');
+const months = document.querySelectorAll('[data-period]');
 const monthsInput = document.getElementById('months-input');
 const monthsRange = document.getElementById('months-range');
 
+const payments = document.getElementById('payments');
+const rateText = document.getElementById('rate-text')
+
 depositInput.addEventListener('input', () => calc(depositInput, depositRange, deposit));
 monthsInput.addEventListener('input', () => calc(monthsInput, monthsRange, months));
+payments.addEventListener('click', changeRate);
 
 function calc(input, range, text) {
   const min = parseInt(input.min);
@@ -15,5 +19,17 @@ function calc(input, range, text) {
   const val = parseInt(input.value);
   const percent = (val - min) / (max - min) * 100;
   range.style.width = percent + '%';
-  text.innerText = val
+  if (text.length) {
+    text.forEach((item) => item.innerText = val)
+  } else {
+    text.innerText = val
+  }
+}
+
+function changeRate() {
+  const rates = [19.8, 20.6, 21, 25]
+  const payments = [...document.querySelectorAll('[data-payment]')];
+  const index = payments.findIndex((item) => item.classList.contains("active"))
+
+  rateText.innerText = rates[index]
 }
--- js/form.js
+++ js/form.js
@@ -1,24 +1,89 @@
-const birthInput = document.getElementById('birth');
+document.addEventListener('DOMContentLoaded', () => {
+  const birthInput = document.getElementById('birth');
+  const nameInput = document.querySelector('.name');
+  const phoneInput = document.querySelector('.phone');
 
-birthInput.addEventListener('input', function() {
-  const value = this.value.replace(/\D/g, ''); // Remove non-digit characters
-  const formattedValue = value.replace(/(\d{2})(\d{2})(\d{4})/, '$1.$2.$3'); // Format the value
+  const payments = document.querySelectorAll('[data-payment]');
+  const citizenships = document.querySelectorAll('[data-citizen]');
 
-  if (formattedValue.length > 10) {
-    this.value = formattedValue.slice(0, 10); // Limit the input to 10 characters
-  } else {
-    this.value = formattedValue;
+  const calcForm = document.getElementById('calc-form');
+
+  payments.forEach((item, _, array) => toggleRadio(item, 'payment', array))
+  citizenships.forEach((item, _, array) => toggleRadio(item, 'citizen', array))
+
+  nameInput.addEventListener('input', function () {
+    // Get the value of the input field
+    const inputValue = this.value;
+
+    // Check if the input value matches the allowed pattern
+    const isValid = /^[ A-Za-zА-Яа-яЁё]+$/.test(inputValue);
+
+    // If the input value is not valid, clear the input field
+    if (!isValid) {
+      nameInput.value = inputValue.replace(/[^ A-Za-zА-Яа-яЁё]/g, '');
+    }
+  })
+
+  phoneInput.addEventListener('focus', function () {
+    // Set the value to start with '+7' when the input is in focus
+    if (!phoneInput.value.startsWith('+7')) {
+      phoneInput.value = '+7';
+    }
+  });
+
+  phoneInput.addEventListener('input', (event) => {
+    let inputValue = event.target.value;
+    inputValue = inputValue.replace(/[^\d]/g, ""); // удаляем все не цифры
+    if (inputValue.length > 11) {
+      inputValue = inputValue.substr(0, 11); // обрезаем до 11 символов
+    }
+    inputValue = "+7" + inputValue.substr(1); // добавляем +7 в начало
+    event.target.value = inputValue;
+  });
+
+  birthInput.addEventListener('input', (event) => {
+    const value = event.target.value.replace(/\D/g, ''); // Remove non-digit characters
+    const formattedValue = value.replace(/(\d{2})(\d{2})(\d{4})/, '$1.$2.$3'); // Format the value
+
+    if (formattedValue.length > 10) {
+      event.target.value = formattedValue.slice(0, 10); // Limit the input to 10 characters
+    } else {
+      event.target.value = formattedValue;
+    }
+  });
+
+  birthInput.addEventListener('focus', clearDate);
+  birthInput.addEventListener('blur', clearDate);
+
+  calcForm.addEventListener('submit', submitForm);
+
+  function toggleRadio(item, dataValue, array) {
+    item.addEventListener('click', () => {
+      array.forEach((btn) => {
+        btn.classList.remove('active')
+        btn.dataset[dataValue] = ''
+      })
+
+      item.classList.add('active')
+      item.dataset[dataValue] = 'active'
+      document.getElementById(dataValue).value = item.querySelector('span').innerText
+    })
   }
-});
 
-birthInput.addEventListener('focus', function() {
-  if (this.value === '') {
-    this.value = 'DD.MM.YYYY';
+  function clearDate(event) {
+    if (event.target.value === 'ДД.ММ.ГГГГ') {
+      event.target.value = '';
+    }
   }
-});
 
-birthInput.addEventListener('blur', function() {
-  if (this.value === 'DD.MM.YYYY') {
-    this.value = '';
+  function submitForm(e) {
+    e.preventDefault();
+
+    const formData = new FormData(e.target);
+
+    // Log the form data to the console
+    for (let pair of formData.entries()) {
+      console.log(pair[0] + ': ' + pair[1]);
+    }
   }
-});
+})
