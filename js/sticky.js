03c94c83d52b6fa8fb08ee35bf684de207befb4b
JediKnightGIT <jediknightru@gmail.com>
2024-09-17 16:29:37 +0300

    new icon, better imgs, mobile footer, fix margins

--- js/sticky.js
+++ js/sticky.js
@@ -1,26 +1,29 @@
 if (isMobile.any()) {
   const stickyElement = document.querySelector('.sticky');
   const stickyBg = document.querySelector('.sticky-bg');
+  window.addEventListener('scroll', () => sticky);
+  window.addEventListener('touchmove', () => sticky);
+
+  const openDepositBtn = document.getElementById('open-deposit-fixed');
   window.addEventListener('scroll', () => {
+    if (window.scrollY > 0) {
+      openDepositBtn.classList.remove('hidden');
+      openDepositBtn.classList.add('block');
+    } else {
+      openDepositBtn.classList.add('hidden');
+      openDepositBtn.classList.remove('block');
+    }
+  });
+
+  function sticky() {
     const stickyRect = stickyElement.getBoundingClientRect();
 
-    if (stickyRect.top <= 0) {
+    if (stickyRect.top === 0) {
       stickyElement.classList.add('stuck');
       stickyBg.classList.add('stuck');
     } else {
       stickyElement.classList.remove('stuck');
       stickyBg.classList.remove('stuck');
     }
-  });
-}
-
-const openDepositBtn = document.getElementById('open-deposit-fixed');
-window.addEventListener('scroll', () => {
-  if (window.scrollY > 0) {
-    openDepositBtn.classList.remove('hidden');
-    openDepositBtn.classList.add('block');
-  } else {
-    openDepositBtn.classList.add('hidden');
-    openDepositBtn.classList.remove('block');
   }
-});
+}
