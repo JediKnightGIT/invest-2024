6ae1499afc9726d4ad889f7df4b4785359bfa993
JediKnightGIT <jediknightru@gmail.com>
2024-09-17 16:49:35 +0300

    sticky fix 4, date fix

--- js/sticky.js
+++ js/sticky.js
@@ -1,28 +1,11 @@
 if (isMobile.any()) {
   const stickyElement = document.querySelector('.sticky');
   const stickyBg = document.querySelector('.sticky-bg');
-  window.addEventListener('scroll', () => {
-    const stickyRect = stickyElement.getBoundingClientRect();
-
-    if (stickyRect.top <= 0) {
-      stickyElement.classList.add('stuck');
-      stickyBg.classList.add('stuck');
-    } else {
-      stickyElement.classList.remove('stuck');
-      stickyBg.classList.remove('stuck');
-    }
-  });
-  window.addEventListener('touchmove', () => {
-    const stickyRect = stickyElement.getBoundingClientRect();
-
-    if (stickyRect.top <= 0) {
-      stickyElement.classList.add('stuck');
-      stickyBg.classList.add('stuck');
-    } else {
-      stickyElement.classList.remove('stuck');
-      stickyBg.classList.remove('stuck');
-    }
-  });
+  window.addEventListener('scroll', sticky);
+  window.addEventListener('touchmove', sticky);
+  window.addEventListener('touchstart', sticky);
+  window.addEventListener('touchend', sticky);
+  window.addEventListener('touchcancel', sticky);
 
   const openDepositBtn = document.getElementById('open-deposit-fixed');
   window.addEventListener('scroll', () => {
